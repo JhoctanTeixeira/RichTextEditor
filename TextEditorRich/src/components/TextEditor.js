7eaea0dba250d5c4798edd636b7505b491552038
Jhoctan Teixeira <rm88206@fiap.com.br>
2022-01-18 13:05:35 -0300

    TextEditor e inde.js criado

---
+++ TextEditorRich/src/components/TextEditor.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Editor } from "slate";
+import { Value } from "slate";
+
+export default class TextEditor extends Component() {
+  state = {
+    value: "",
+  };
+
+  onChange = ({ value }) => {
+    this.setState({
+      value,
+    });
+  };
+  render() {
+    return(
+        <Editor value={this.state.value} onChange={this.onChange} />
+    )
+  }
+}
